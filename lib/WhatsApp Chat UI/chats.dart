ccd35e98de3f556f732523e7aea1a4b4c6990f3a
alameen-developer <alameendeveloperspace@gmail.com>
2023-05-22 16:04:33 +0530

    initial commit

--- lib/WhatsApp Chat UI/chats.dart
+++ lib/WhatsApp Chat UI/chats.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(DevicePreview(
-      builder: (BuildContext context) => MaterialApp(
-            home: chats_screen(),
-            useInheritedMediaQuery: true,
-          )));
+    builder: (BuildContext context) => MaterialApp(
+      home: chats_screen(),
+      debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      theme: ThemeData(primarySwatch: Colors.teal),
+    ),
+  ));
 }
 
 class chats_screen extends StatelessWidget {
@@ -24,22 +27,68 @@ class chats_screen extends StatelessWidget {
     'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
     'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   ];
-  var names = ['Ameen','Kavya','Kuru','Koos','keerthi','Ameen','Kavya','Kuru','Koos','keerthi'];
-  var messages = ['hi','hello','how are you?','ok','bye','hi','hello','how are you?','ok','bye'];
-  var time = ['02:34','12:34','5:05','02:34','12:34','5:05','02:34','12:34','5:05','07:00'];
+  var names = [
+    'Ameen',
+    'Kavya',
+    'Kripa',
+    'Kurian',
+    'keerthi',
+    'Ameen',
+    'Kavya',
+    'Kasim',
+    'Kripa',
+    'keerthi'
+  ];
+  var messages = [
+    'hi',
+    'hello',
+    'how are you?',
+    'ok',
+    'bye',
+    'hi',
+    'hello',
+    'how are you?',
+    'ok',
+    'bye'
+  ];
+  var time = [
+    '02:34',
+    '12:34',
+    '5:05',
+    '02:34',
+    '12:34',
+    '5:05',
+    '02:34',
+    '12:34',
+    '5:05',
+    '07:00'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('WhatsApp'),
+        actions: const [
+          Icon(Icons.search),
+          SizedBox(width: 20),
+          Icon(Icons.add),
+          SizedBox(width: 20),
+        ],
+      ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(dps[index]),
+          return Container(height: 100,
+            child: Center(
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(dps[index]),
+                ),
+                title: Text(names[index]),
+                subtitle: Text(messages[index]),
+                trailing: Text(time[index]),
+              ),
             ),
-            title: Text(names[index]),
-            subtitle: Text(messages[index]),
-            trailing: Text(time[index]),
           );
         },
         itemCount: 10,
--- lib/simple_splashscreen_design.dart
+++ lib/simple_splashscreen_design.dart
@@ -1,13 +1,13 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(DevicePreview(
-    builder: (BuildContext context) =>const MaterialApp(
+    builder: (BuildContext context) => const MaterialApp(
       home: simple_splashscreen(),
       useInheritedMediaQuery: true,
-      debugShowCheckedModeBanner: false
-      ,
+      debugShowCheckedModeBanner: false,
     ),
   ));
 }
@@ -18,14 +18,45 @@ class simple_splashscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [Colors.purple, Colors.pink, Colors.orange],
-        )),
-        child: Center(child: Column(children: [Image.network('src')],),),
+        body: Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Colors.orange, Colors.pink, Colors.purple],
+      )),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://cdn-icons-png.flaticon.com/256/9846/9846996.png',
+              width: 150,
+              height: 150,
+            ),
+            RichText(
+              text: TextSpan(
+                  text: 'Tivi',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'dey',
+                      style: GoogleFonts.amaranth(fontWeight: FontWeight.normal,color: Colors.cyan),
+
+                      // TextStyle(
+                      //   fontFamily: 'DancingScript',
+                      //   fontWeight: FontWeight.normal,
+                      // ),
+                    )
+                  ]),
+            )
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
